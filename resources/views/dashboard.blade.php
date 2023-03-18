3b2d599931bcd5b8558339be46623c5b966608d3
Andrew Dev <aniq.dev@gmail.com>
2023-03-18 20:03:32 +0200

    auth

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-2">
+            Sidebar
+        </div>
+        <div class="col-md-10">
+            Main
+        </div>
+    </div>
+</div>
+@endsection
