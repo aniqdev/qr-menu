87e89c049e3ae3ddfd4cadf9f2f76cdbe10b25d2
aniq.dev <aniq.dev@gmail.com>
2023-09-12 13:53:56 +0300

    landing title

--- resources/views/qrsaas/home.blade.php
+++ resources/views/qrsaas/home.blade.php
@@ -1,20 +1,3 @@
-<!--
-
-=========================================================
-* Impact Design System - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/impact-design-system
-* Copyright 2010 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/impact-design-system/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
--->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,7 +9,7 @@
     <link rel="icon" type="image/png" href="{{ asset('argonfront') }}/img/favicon.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta property="og:image" content="{{ logo_src() }}">
-    <title>{{ config('global.site_name','QRTiger') }}</title>
+    <title>QR-Menu</title>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     @if (config('settings.google_analytics'))
